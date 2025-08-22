7d11aa046c9af34184a44a01f79dca5f5749f27e
shade2go <arijitdutta4006@gmail.com>
2025-08-22 14:50:59 +0530

    new update
    added day date month and year

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
 function updateclock() {
     const timern = new Date()
     document.getElementById("mainclock").innerText = timern.toLocaleTimeString();
+    listofdays = ["Sunday","Monday", "Tuesday","Wednesday","Thursday","Friday","Saturday"];
+    dayrn = listofdays[timern.getDay()];
+
+    listofmonths = ["January", "Febuary", "March", "April", "May", "June", "July", "August", "September","October","November","December"];
+    monthrn = listofmonths[timern.getMonth()]
+    document.getElementById("fulldate").innerText = `${dayrn}, ${timern.getDate()} ${monthrn}, ${timern.getFullYear()}`
 }
 setInterval(updateclock, 1000)
 updateclock()
@@ -14,3 +20,5 @@ shwatermarkbutton.addEventListener("click", function showhide() {
     isvisible = !isvisible
     isvisible ? signature.style.display = "block" : signature.style.display = "none"
 });
+
+document.getElementById("timezone").innerText = `Your Timezone: ${new Date().toString().slice(24)}`
