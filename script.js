7304ee2ed908f983d3012c672f46f9ce3cb5df84
shade2go <arijitdutta4006@gmail.com>
2025-08-25 23:01:46 +0530

    new upd
    light dark mode

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ function updateclock() {
     listofdays = ["Sunday","Monday", "Tuesday","Wednesday","Thursday","Friday","Saturday"];
     dayrn = listofdays[timern.getDay()];
 
-    listofmonths = ["January", "Febuary", "March", "April", "May", "June", "July", "August", "September","October","November","December"];
+    listofmonths = ["January", "February", "March", "April", "May", "June", "July", "August", "September","October","November","December"];
     monthrn = listofmonths[timern.getMonth()]
     document.getElementById("fulldate").innerText = `${dayrn}, ${timern.getDate()} ${monthrn}, ${timern.getFullYear()}`
 }
@@ -28,3 +28,11 @@ document.getElementById("fullscreenbutton").addEventListener("click", () =>
         ? document.exitFullscreen ? document.exitFullscreen() : document.webkitExitFullscreen ? document.webkitExitFullscreen() : document.msExitFullscreen && document.msExitFullscreen()
         : document.documentElement.requestFullscreen ? document.documentElement.requestFullscreen() : document.documentElement.webkitRequestFullscreen ? document.documentElement.webkitRequestFullscreen() : document.documentElement.msRequestFullscreen && document.documentElement.msRequestFullscreen()
 );
+
+let isdark = true
+document.getElementById("darklighttoggle").addEventListener("click", function darklighttoggle() {
+    isdark = !isdark
+    isdark ? document.getElementById("cssselector").href = "style.css" : document.getElementById("cssselector").href = "lightstyle.css"
+    isdark ? document.getElementById("darklighttoggle").innerText = "Light Mode" : document.getElementById("darklighttoggle").innerText = "Dark Mode"
+});
+
